9849af9b1d2f012f44bacb811a1e019d62357b3d
Uwe Ligges <ligges@statistik.uni-dortmund.de>
2006-02-14 20:51:54 +0000

    version 0.2-7

--- R/model.get.seed.R
+++ R/model.get.seed.R
@@ -1,10 +1,11 @@
 "modelGetSeed" <-
-function()
+function(i = 1)
 #   Get the seed of random number generator
 {
-    command <- "BugsEmbed.GetSeed"
-    .C("CmdInterpreter", command, nchar(command), integer(1), PACKAGE="BRugs")
-    command <- "BugsEmbed.newSeed"
-    as.integer(.C("Integer", command, nchar(command), 
-        integer(1), integer(1), PACKAGE="BRugs")[[3]])
+    if(!is.numeric(i))
+        stop("i ", "must be numeric")
+    command <- paste("BugsEmbed.index := ", as.integer(i), ";BugsEmbed.GetRNState")
+    res <- .C("CmdInterpreter", command, nchar(command), integer(1), PACKAGE="BRugs")[[3]]
+    if(res) stop("Getting seed returned with an error.")
+    buffer()
 }
--- R/model.set.seed.R
+++ R/model.set.seed.R
@@ -1,12 +1,15 @@
-"modelSetSeed" <-
+"modelSetSeed" <- 
 function(newSeed)
 #   Set the seed of random number generator
 {
     if(!is.numeric(newSeed))
         stop("newSeed ", "must be numeric")
     newSeed <- as.integer(newSeed)
-    command <- paste("BugsEmbed.newSeed := ", newSeed)
-    .C("CmdInterpreter", command, nchar(command), integer(1), PACKAGE="BRugs")
-    command <- "BugsEmbed.SetSeedGuard; BugsEmbed.SetSeed"
-    invisible(.C("CmdInterpreter", command, nchar(command), integer(1), PACKAGE="BRugs"))
-}
+    for(i in seq(along=newSeed)){
+        command <- paste("BugsEmbed.index :=", i, "; BugsEmbed.new :=", newSeed[i], 
+            ";BugsEmbed.SetRNGuard; BugsEmbed.SetRNState")
+        res <- .C("CmdInterpreter", command, nchar(command), integer(1), PACKAGE="BRugs")[[3]]
+    }
+    if(!res) cat("Seed successfully set\n")
+    else stop("Setting seed returned with an error.")
+}    
