2b2f6b5a8e7a0d09f8519cbaa8273bf572d0b4c8
Uwe Ligges <ligges@statistik.tu-dortmund.de>
2012-07-24 16:09:15 +0000

    version 0.8-0

--- R/info.modules.R
+++ R/info.modules.R
@@ -5,7 +5,7 @@
     command <- "BugsEmbed.Modules"
     .CmdInterpreter(command)
     buffer <- file.path(tempdir(), "buffer.txt")
-    result <- read.fwf(buffer, c(50, 12, 12, 12, 12, 10), skip = 1, as.is=TRUE)
+    result <- read.table(buffer, skip = 1, as.is=TRUE, sep="\t")[,-1]
     for(i in c(1,4,5,6))
         result[,i] <- gsub(" ", "", result[,i])
     names(result) <- c("Module", "Clients", "Version", "Maintainer", "Compiled", "Loaded")
